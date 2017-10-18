1e8b6b7c2e4e9fae21fe8debe15db836261ef82f
hjoshi123 <hemant2612@hotmail.com>
2017-10-18 16:02:41 +0530

    Update MNIST_IMAGE.py

--- MNIST_IMAGE.py
+++ MNIST_IMAGE.py
@@ -4,8 +4,11 @@ from matplotlib import pyplot as plt
 from random import randint
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+#Getting a random image from the MNIST test images using random number
 num = randint(0, mnist.test.images.shape[0])
 img = mnist.test.images[num]
+
 #tf.placeholder(tf.float32,[]) tf.float32 is the 32 bit storage fo Cr computation graph
 #[] is for the dimensions of input In this case since its images. No of images is unknown this None as first then the RGB values 
 #for color images. Since its grayscale therefore 28*28*1=784 works.
@@ -39,7 +42,7 @@ for i in range(1000):
     train_data = {X: batch_X, Y_: batch_Y}
     sess.run(train_step, feed_dict=train_data)
     a,c = sess.run([accuracy, cross_entropy], feed_dict=train_data)
-    
+    #a means accruracy c means cross_entropy i.e cost function
     #print(a)
     #print(c)
     
@@ -48,9 +51,11 @@ for i in range(1000):
     #print(a)
     #print(c)   
 
+#Prediction
 classification = sess.run(tf.argmax(Y, 1), feed_dict={X: [img]})
+
+#Plotting the image that is used for prediction
 plt.imshow(img.reshape(28, 28), cmap=plt.cm.binary)
 plt.show()
+
 print('NN predicted', classification[0])
-#prediction=tf.argmax(Y,1)
-#print("predictions", prediction.eval(feed_dict={X: mnist.test.images}, session=sess))
